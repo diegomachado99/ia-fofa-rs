b0a3fb445b5cc4a44c8877124c6bd1be4be36aa8
diegomachado99 <160793872+diegomachado99@users.noreply.github.com>
2025-09-11 22:57:17 +0000

    ok

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaprincipal=document.querySelector(",caixa-principal");
+const caixapergutas=ducument.querySelector (",caixa-perguntas");
+const caixaAlternativas=document.querySelector(",caixa-alternativas");
+const caixaResultado=document.querySelector("caixa-reultado");
+const textoresultado=document.querySelector(",texto-resultado");
+ 
+
+const perguntas=[
+    {
+        enunciado:Assim que saiu do colégio voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode Ter, ele tambem gera imagens e audios relistas,qual e seu pensamento",
+        alternativas:[
+            { 
+                texto:" isso e assustador";
+                afirmacao:"no inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+            },
+            {
+                texto:"isso e incrivel"
+                afirmacao:"sempe quis saber como usar uma ia em meu "
+            }
+        ]
+    }
+]
+
